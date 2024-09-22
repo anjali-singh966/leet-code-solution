1c9ea28e120d2251ac29c4c5469921b3c26165f5
Anjali kumari <anjali_223005@saitm.org>
2024-09-22 19:39:18 +0530

    Time: 1 ms (52.07%), Space: 40.4 MB (94.54%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+   
+        public static void main(String[] args)
+        {
+            int num=38;
+            System.out.println(addDigits(num));
+        }
+        public static int addDigits(int num)
+        {
+            if(num==0)
+            {
+                return 0;
+            }
+            return(num%9==0)?9:num%9;
+        }
+    }
